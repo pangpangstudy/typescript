4af80e8bead34ec71117cbbbba7847469b4a9fc7
Zzh <3169427572@qq.com>
2024-08-09 21:45:11 +0800

    重新构造

---
+++ changeType.ts
@@ -0,0 +1,28 @@
+// Push
+type tuple = [1, 2, 3];
+type Push<Arr extends unknown[], Ele> = [...Arr, Ele];
+// Unshift
+type Unshift<Arr extends unknown[], Ele> = [Ele, ...Arr];
+// 合并元组
+type Zip<
+  One extends [unknown, unknown],
+  Other extends [unknown, unknown]
+> = One extends [infer OneFirst, infer OneSecond]
+  ? Other extends [infer OtherFirst, infer OtherSecond]
+    ? [[OneFirst, OtherFirst], [OneSecond, OtherSecond]]
+    : []
+  : [];
+
+type Zip2<One extends unknown[], Other extends unknown[]> = One extends [
+  infer OneFirst,
+  ...infer OneRest
+]
+  ? Other extends [infer OtherFirst, ...infer OtherRest]
+    ? [[OneFirst, OtherFirst], ...Zip2<OneRest, OtherRest>]
+    : []
+  : [];
+// 首字母大写
+type CapitalizeStr<Str extends string> =
+  Str extends `${infer First}${infer Rest}`
+    ? `${Uppercase<First>}${Rest}`
+    : Str;
